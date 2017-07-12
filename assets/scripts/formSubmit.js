0b2767a7e59e3651bac14a1529cd781b40becb53
MrFox-L <feralseattle@gmail.com>
2017-07-11 21:21:03 -0400

    Front end validation, error display.

--- assets/scripts/formSubmit.js
+++ assets/scripts/formSubmit.js
@@ -1,5 +1,34 @@
-$(document).ready();
+$(document).ready(function() {
+
 
 //  Declare variables
 
-var $username = $("input[value='username']");
+var $username = $("input[name='username']");
+var $password = $("input[name='password']");
+var $confirm = $("input[name='confirm']");
+var $submit = $("button");
+var $formUsername = $(".js-username");
+var $formPassword = $(".js-password");
+var $formConfirm = $(".js-confirm");
+
+
+$submit.on("click", function(event) {
+		event.preventDefault();
+		if (!$username.val()) {
+			console.log("$username is null");
+			$formUsername.removeClass("hidden");
+		}
+		if (!$username.val()) {
+			console.log("$password is null");
+			$formPassword.removeClass("hidden");
+		}
+		if ($confirm.length && !$confirm.val()) {
+			console.log("$confirm is null");
+			$formConfirm.removeClass("hidden");
+		}
+
+});
+
+
+
+});
