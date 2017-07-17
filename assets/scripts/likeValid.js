2ab41067dd5a5d96f580b3f611335b70258bceed
MrFox-L <feralseattle@gmail.com>
2017-07-16 20:57:12 -0400

    Minor styling changes.

--- assets/scripts/likeValid.js
+++ assets/scripts/likeValid.js
@@ -2,10 +2,11 @@ $(document).ready(function() {
 
 	// var userid = $(".js-userid").html();
 	var fileUserIds = $(".userid");
-	var like = $(".like-box");
+	var like = $(".like-img-form button");
+	var likeImg = $(".like-img-form img");
 
-	console.log(userId);
-	console.log(fileUserId);
+	// console.log(userId);
+	// console.log(fileUserId);
 
 	// for each on an array of photo.usernames 
 	// $.each(fileUserIds, function() {
@@ -14,6 +15,11 @@ $(document).ready(function() {
 	// 	}
 	// });
 
+	like.on("click", function(event) {
+		event.preventDefault();
+		console.log("Click", event.target);
+		$(event.target).attr("src", "../images/liked.png");
+	});
 
 
 
