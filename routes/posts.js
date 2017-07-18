e41a090b216c1a80da4336570100b12b94448faf
Alex Polotsky <polonyc77@gmail.com>
2017-07-18 18:25:45 -0400

    comments-current-user

--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,7 @@ const Gram = ("./util/gram");
 const router = exp.Router();
 const fs = require("fs-extra");
 const Files = require("../models/file");
-
+const Comment = require("../models/comment");
 const multer = require("multer");
 const uploader = multer({
 	dest: "uploads/"
@@ -26,7 +26,7 @@ router.post("/", uploader.single("image"), function(req, res) {
 	//Otherwise, try an upload
 	req.user.upload(req.file, req.body).then(function(data) {
 			// res.json(data)
-			console.log(data, "kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk")
+			console.log(data, "kkkkkkkkkkkkkkkkkkkkkkkkkkkk")
 			res.redirect("/form/" + data.id + "/description")
 		})
 		.catch(function(err) {
@@ -36,127 +36,136 @@ router.post("/", uploader.single("image"), function(req, res) {
 			});
 		});
 })
-			
+
 router.get("/:id/description", function(req, res) {
 	res.render("description")
 })
 
 router.post("/:id/description", function(req, res) {
 	Files.findOne({
-			where: {
-				id: req.params.id
-			}
-		}).then(function(file) {
+		where: {
+			id: req.params.id,
+		}
+	}).then(function(file) {
+			
+		if (file.userId === req.session.userid) {
 			file.update({
 				description: req.body.description
+			}).then(function() {
+
+				res.redirect("/form/gram/");
 			})
-			res.redirect("/form/gram")
-		}).catch(function(err) {
-			console.error("Something went wrong with upload", err);
-		})
-		//description: req.body.description
-})
+	 	} 
+	}).catch(function(err) {
+		console.error("Something went wrong with upload", err);
+	   res.redirect("/form/gram/");
 
-
-//Render an individual document
-
-router.get("/gram/:fileId", function(req, res) {
-	Files.findById(req.params.fileId).then(function(file) {
-			// console.log(file, ")))))))))))))))))))))))))))))))))))))))))))))))))))))((((((((((((")
-			if (file) {
-				res.render(file.get("id"), "gram", {
-					files: file,
-				});
-			} else {
-				res.status(404);
-				res.render("Not Found", "404");
-			} // console.log(file, ")))))))))))))))))))))))))))))))))))))))))))))))))))))((((((((((((")
-		})
-		.catch(function(err) {
-			console.error("Error while fetching file " + req.params.fileId, err);
-			res.status(500).send("Something went wrong!")
-		});
+	 });
 });
 
-router.get("/gram", function(req, res) {
-	Files.findAll({ order: [['createdAt', 'DESC']] }).then(function(file) {
-		// console.log(file, "9999999999999999999999999999999999999999999999999999999999999999999999999999999999")
-		res.render("gram", { 
-// router.get("/gram/:fileId", function(req, res) {
-// 	Files.findById(req.params.fileId).then(function(file) {
-// 			console.log(file, ")))))))))))))))))))))))))))))))((((((((((((")
-// 			if (file) {
-// 				res.render(file.get("id"), "gram", {
-// 					files: file,
-// 				});
-// 			} else {
-// 				res.status(404);
-// 				res.render("Not Found", "404");
-// 			}console.log(file, ")))))))))))))))))))))))))))))))))))))))))))))))))))))((((((((((((")
-// 		})
-// 		.catch(function(err) {
-// 			console.error("Error while fetching file " + req.params.fileId, err);
-// 			res.status(500).send("Something went wrong!")
-// 		});
-// });
+router.post("/comment", function(req, res) {
+	if (!req.body.fileId || !req.body.comment) {
+		return res.status(500).send("Missing required comment field");
+	}
+	Files.findById(req.body.fileId).then(function(file) {
+		if (file) {
+			file.createComment({
+					text: req.body.comment,
+					userId: req.session.userid,
+				})
+				// .then(function(comment) {
+				//     req.user.addComment({
+				//     	userId: userId,
+				//     })
+			res.redirect("/form/gram");
+			// });
+		} else {
+			res.render(res, "404");
+		}
+	});
+});
 
 router.get("/gram", function(req, res) {
 	Files.findAll({
-		order: [
-			['createdAt', 'DESC']
-		]
-	}).then(function(file) {
-		console.log(file, "999999999999999999999999999999999999999999")
+		include: [Comment]
+	}).then(function(files) {
+		//console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.\n', JSON.stringify(files), '\n>>>>>>>>>>>>>>>>>>>>>>>>>>\n')
 		res.render("gram", {
->>>>>>> gram
-			files: file,
-
+			files: files,
 		});
+
 	});
 });
-<<<<<<< HEAD
-		
-// Sample routes for displaying user & file content
-// test route for creating a like for a photo
 
-router.post("/like/:fileid", function(req, res) {
-	console.log(req.params.fileid);
-	Files.findById(req.params.fileid)
-	.then(function(file) {
-		if (file && file.userid !== req.user.id) {
+router.post("/delete", function(req, res) {
+	// console.log(req.session.userid, "ooooooooooooYYYYYYYYYYYYYYYYooooooooo")
+	// console.log(req.body.fileId)
+	Files.findById(req.body.fileId).then(function(file) {
+		console.log(file.userId, "lllllllllllllllllPPPPPPPPPPPPPPPPPPPPPPPPiiiiiiiiii")
 
-			req.user.like(req.params.fileid)
-			.then(function(like) {
-				res.redirect("../../form/gram");
+		if (file.userId === req.session.userid) {
+			// if (file) {
+			file.destroy({
+				fileId: req.body.fileId,
+
 			})
-			.catch(function(err) {
-				console.error(err);
-			})
-			
+			res.redirect("/form/gram")
+
+			// }
 		}
-	})
-})
+	});
+});
 
+// router.post("/comment", function(req, res) {
+// 			Files.findById(req.body.fileId).then(function(file) {
+// 				//console.log(req.body.field, "gggggggggggggggggggggg")
+// 				if (!req.body.fileId || !req.body.comment) {
+// 					return res.status(500).send("Missing required comment field");
+// 				}
+// 				if (req.body.btn == "add") {
+// 					Files.findById(req.body.fileId).then(function(file) {
+// 						//console.log(req.session.userid, "gggggggggggggggggggggg")
+// 						if (file) {
+// 							file.createComment({
+// 									text: req.body.comment,
+// 									userId: req.session.userid,
+// 								})
+// 								// .then(function(comment) {
+// 								//     req.user.addComment({
+// 								//     	userId: userId,
+// 								//     })
+// 							res.redirect("/form/gram/");
+// 							// });
+// 						} else {
+// 							res.render(res, "404");
+// 						}
 
+// 					})
+// 				} else {
+// 					Files.findById(req.body.fileId).then(function(file) {
+// 						if (file) {
 
+// 							file.destroy({
+// 								fileId: req.body.fileId
+// 							})
+// 							res.render("gram")
+// 						}
+// 					})
+// 				}
+// 			})
+// })
+// router.get("/gram", function(req, res) {
+// 	Files.findAll({
+// 		order: [
+// 			['createdAt', 'DESC']
+// 		]
+// 	}).then(function(file) 
+// 		//console.log(file, "999999999999999999999999999999999999999999")
+// 		res.render("gram", {
+// 			files: file,
 
-// Download a document, if it exists
-// router.get("/download/:fileId", function(req, res) {
-// 	File.findById(req.params.fileId).then(function(file) {
-// 			if (file) {
-// 				res.download("uploads/" + file.get("id"), file.get("originalName"));
-// 			} else {
-// 				res.status(404).send("No file found");
-// 			}
-// 		})
-// 		.catch(function(err) {
-// 			console.error(err);
-// 			res.status(500).send("Something went wrong");
 // 		});
+// 	});
 // });
 
-
-
 module.exports = router;
-
-
